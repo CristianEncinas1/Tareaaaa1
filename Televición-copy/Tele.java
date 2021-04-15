8916eab20ae94db37f0c0a40ed1dd077d5adb9dd
Cristian Encinas <81886765+CristianEncinas1@users.noreply.github.com>
2021-04-14 21:35:03 -0400

    Add files via upload
    Auxi aquí está mi humilde tarea , espero que este bien

---
+++ Televición-copy/Tele.java
@@ -0,0 +1,20 @@
+
+public class Tele
+{ 
+    private int TamañoDeTele; 
+    private int ModeloDeTele;
+    private int color;
+    private int calidadDeMaterial;
+
+    // Aquí pondré tolo lo que quiero que sea publico y este es mi constructor 
+     public Tele (String calidadDeImagen, int tamaño ,int pulgadasDeTele, int Garantía ){
+    int thTamaño = 52 ;
+    int GarantiaAnual  = 1;
+    int PulgadasDeTele = 50;
+    int CalidadDeImagen = 10 ;
+    
+    
+    
+    }
+    
+}
